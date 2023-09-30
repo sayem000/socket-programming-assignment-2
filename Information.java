9ba58f57f47e58d254dcf8f760a5ceed65c96d5a
sayem000 <105208870+sayem000@users.noreply.github.com>
2023-10-01 00:02:41 +0600

    Add files via upload

---
+++ Information.java
@@ -0,0 +1,10 @@
+
+public class Information {
+    public String username;
+    public NetworkConnection netConnection;
+    public Information(String user, NetworkConnection nConnection){
+        username=user;
+        netConnection=nConnection;
+    }
+
+}
